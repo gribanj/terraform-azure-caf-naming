b20287f2003952630f593a63662ca78d57d3071e
yuriyg <yuriyg@griban.ga>
2023-11-16 11:50:42 -0500

    updates

--- variables.tf
+++ variables.tf
@@ -12,39 +12,39 @@ variable "settings" {
   }))
 
   default = {
-    rg_name = {
+    rg = {
       resource_type = "azurerm_resource_group"
       suffixes      = ["suffix1", "suffix2"]
     },
-    vnet_name = {
+    vnet = {
       resource_type = "azurerm_virtual_network"
       suffixes      = ["suffix1", "suffix2"]
     },
-    snet_name = {
+    snet = {
       resource_type = "azurerm_subnet"
       suffixes      = ["suffix1", "suffix2"]
     },
-    nsg_name = {
+    nsg = {
       resource_type = "azurerm_network_security_group"
       suffixes      = ["suffix1", "suffix2"]
     },
-    nsgr_name = {
+    nsgr = {
       resource_type = "azurerm_network_security_rule"
       suffixes      = ["suffix1", "suffix2"]
     },
-    avail_name = {
+    avail = {
       resource_type = "azurerm_availability_set"
       suffixes      = ["suffix1", "suffix2"]
     },
-    pip_name = {
+    pip = {
       resource_type = "azurerm_public_ip"
       suffixes      = ["suffix1", "suffix2"]
     },
-    nic_name = {
+    nic = {
       resource_type = "azurerm_network_interface"
       suffixes      = ["suffix1", "suffix2"]
     },
-    aks_name = {
+    aks = {
       resource_type = "azurerm_kubernetes_cluster"
       suffixes      = ["suffix1", "suffix2"]
     },
