05325d79433a17ad1a3f0e50ce8ada2d0a104a0b
Yuriy Gribanov <83383665+gribanj@users.noreply.github.com>
2023-12-26 23:25:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,75 +14,75 @@ variable "settings" {
   default = {
     rg = {
       resource_type = "azurerm_resource_group"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     vnet = {
       resource_type = "azurerm_virtual_network"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     snet = {
       resource_type = "azurerm_subnet"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     nsg = {
       resource_type = "azurerm_network_security_group"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     nsgr = {
       resource_type = "azurerm_network_security_rule"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     avail = {
       resource_type = "azurerm_availability_set"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     pip = {
       resource_type = "azurerm_public_ip"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     nic = {
       resource_type = "azurerm_network_interface"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     pdns = {
       resource_type = "azurerm_private_dns_zone"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     pe = {
       resource_type = "azurerm_private_endpoint"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     bast = {
       resource_type = "azurerm_bastion_host"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     lb = {
       resource_type = "azurerm_lb"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     pep = {
       resource_type = "azurerm_private_endpoint"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     msi = {
       resource_type = "azurerm_user_assigned_identity"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     log = {
       resource_type = "azurerm_log_analytics_workspace"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     kv = {
       resource_type = "azurerm_key_vault"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     nw = {
       resource_type = "azurerm_network_watcher"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     st = {
       resource_type = "azurerm_storage_account"
-      suffixes      = [local.suffix1, local.suffix2]
+      suffixes      = [suffix1, suffix2]
     },
     # Add more resources as needed
   }
